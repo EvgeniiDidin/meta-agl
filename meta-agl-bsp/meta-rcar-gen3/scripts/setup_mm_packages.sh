5875cc45a671b1a3e7671ec84ec6ebb1ab17696a
Ronan Le Martret <ronan.lemartret@iot.bzh>
2017-03-24 15:09:42 +0100

    Update rcar gen3 script, compliant driver update
    Setup_mm_packages.sh must be compliant with driver update.
    Test if EXTRACT_DIR do not exists block the update.
    
    Change-Id: If5a8adfbf187e0894d04488eb40aa6ddbe48ad95
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- meta-agl-bsp/meta-rcar-gen3/scripts/setup_mm_packages.sh
+++ meta-agl-bsp/meta-rcar-gen3/scripts/setup_mm_packages.sh
@@ -9,24 +9,31 @@ test -f ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs && source ${XDG_CONFIG_HOME
 DOWNLOAD_DIR=${XDG_DOWNLOAD_DIR:-$HOME/Downloads}
 EXTRACT_DIR=$METADIR/binary-tmp
 
+
 function copy_mm_packages() {
-        if [ ! -d $EXTRACT_DIR ]; then
-                if [ -f $DOWNLOAD_DIR/$ZIP_1 -a -f $DOWNLOAD_DIR/$ZIP_2 ]; then
-                        mkdir $EXTRACT_DIR
-                        unzip -o $DOWNLOAD_DIR/$ZIP_1 -d $EXTRACT_DIR
-                        unzip -o $DOWNLOAD_DIR/$ZIP_2 -d $EXTRACT_DIR
-                else
-                        echo -n "The graphics and multimedia acceleration packages for "
-                        echo -e "the R-Car Gen3 board can be downloaded from:"
-                        echo -e " https://www.renesas.com/en-us/software/D6000821.html"
-                        echo -e " https://www.renesas.com/en-us/software/D6000822.html"
-                        echo -e
-                        echo -n "These 2 files from there should be stored in your"
-                        echo -e "'$DOWNLOAD_DIR' directory."
-                        echo -e "  $ZIP_1"
-                        echo -e "  $ZIP_2"
-                        return 1
+        if [ -f $DOWNLOAD_DIR/$ZIP_1 -a -f $DOWNLOAD_DIR/$ZIP_2 ]; then
+                mkdir -p $EXTRACT_DIR
+                unzip -n $DOWNLOAD_DIR/$ZIP_1 -d $EXTRACT_DIR
+                if [ $? -ne 0 ]; then
+                    echo -e "ERROR: FILES \""+$DOWNLOAD_DIR/$ZIP_1+"\" NOT EXTRACTING CORRECTLY"
+                    return 1
                 fi
+                unzip -n $DOWNLOAD_DIR/$ZIP_2 -d $EXTRACT_DIR
+                if [ $? -ne 0 ]; then
+                    echo -e "ERROR: FILES \""+$DOWNLOAD_DIR/$ZIP_2+"\" NOT EXTRACTING CORRECTLY"
+                    return 1
+                fi
+        else
+                echo -n "The graphics and multimedia acceleration packages for "
+                echo -e "the R-Car Gen3 board can be downloaded from:"
+                echo -e " https://www.renesas.com/en-us/software/D6000821.html"
+                echo -e " https://www.renesas.com/en-us/software/D6000822.html"
+                echo -e
+                echo -n "These 2 files from there should be stored in your"
+                echo -e "'$DOWNLOAD_DIR' directory."
+                echo -e "  $ZIP_1"
+                echo -e "  $ZIP_2"
+                return 1
         fi
 
         if [ -f $COPY_SCRIPT ]; then
