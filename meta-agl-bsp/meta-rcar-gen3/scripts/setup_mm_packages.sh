4b9d3c542a191c2167b08b32f5cddb41040953c7
Ronan Le Martret <ronan.lemartret@iot.bzh>
2017-05-12 16:40:40 +0200

    Update Renesas gen3 setup script for BSP 2.19.
    * directly use the *.zip file, no need to untar any more.
    
    Bug-AGL: SPEC-605
    
    Change-Id: I970c0f7a7d2eeae05f2a53fd9eca1e0a37916938
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>
    Reviewed-on: https://gerrit.automotivelinux.org/gerrit/9373
    Tested-by: Jenkins Job builder account <agl-jobbuilder@automotivelinux.org>
    Reviewed-by: Changhyeok Bae <changhyeok.bae@lge.com>
    Reviewed-by: Stéphane Desneux <stephane.desneux@iot.bzh>
    Reviewed-by: Jan-Simon Moeller <jsmoeller@linuxfoundation.org>

--- meta-agl-bsp/meta-rcar-gen3/scripts/setup_mm_packages.sh
+++ meta-agl-bsp/meta-rcar-gen3/scripts/setup_mm_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ZIP_1="R-Car_Gen3_Series_Evaluation_Software_Package_for_Linux-20170324.zip"
-ZIP_2="R-Car_Gen3_Series_Evaluation_Software_Package_of_Linux_Drivers-20170324.zip"
+ZIP_1="R-Car_Gen3_Series_Evaluation_Software_Package_for_Linux-20170427.zip"
+ZIP_2="R-Car_Gen3_Series_Evaluation_Software_Package_of_Linux_Drivers-20170427.zip"
 
 COPY_SCRIPT="$METADIR/meta-renesas-rcar-gen3/meta-rcar-gen3/docs/sample/copyscript/copy_evaproprietary_softwares.sh"
 
@@ -13,20 +13,12 @@ EXTRACT_DIR=$METADIR/binary-tmp
 function copy_mm_packages() {
         if [ -f $DOWNLOAD_DIR/$ZIP_1 -a -f $DOWNLOAD_DIR/$ZIP_2 ]; then
                 mkdir -p $EXTRACT_DIR
-                unzip -n $DOWNLOAD_DIR/$ZIP_1 -d $EXTRACT_DIR
-                if [ $? -ne 0 ]; then
-                    echo -e "ERROR: FILES \""+$DOWNLOAD_DIR/$ZIP_1+"\" NOT EXTRACTING CORRECTLY"
-                    return 1
-                fi
-                unzip -n $DOWNLOAD_DIR/$ZIP_2 -d $EXTRACT_DIR
-                if [ $? -ne 0 ]; then
-                    echo -e "ERROR: FILES \""+$DOWNLOAD_DIR/$ZIP_2+"\" NOT EXTRACTING CORRECTLY"
-                    return 1
-                fi
+                cp --update $DOWNLOAD_DIR/$ZIP_1 $EXTRACT_DIR
+                cp --update $DOWNLOAD_DIR/$ZIP_2 $EXTRACT_DIR
         else
                 echo -n "The graphics and multimedia acceleration packages for "
-                echo -e "the R-Car Gen3 board can be downloaded from:"
-                echo -e " <http://www.renesas.com/secret/r_car_download/rcar_demoboard.jsp>"
+                echo -e "the R-Car Gen3 board BSP 2.19 can be downloaded from:"
+                echo -e " <https://www.renesas.com/solutions/automotive/rcar-demoboard.html>"
                 echo -e
                 echo -n "These 2 files from there should be stored in your"
                 echo -e "'$DOWNLOAD_DIR' directory."
