5c19b20245e6bf564c94de641af1881e4f2505d0
Pierre Marzin <pierre.marzin@iot.bzh>
2019-05-28 17:41:19 +0200

    ebisu: Update setup fragment to copy binaries
    Use copy_proprietary_softwares.sh script to manage the zip binaries
    pasted into "$METADIR/binary-tmp".
    
    Bug-AGL: SPEC-2564
    
    Change-Id: I256d1200e03ab9a9482e29d7f42016f99c7ba16c
    Signed-off-by: Pierre Marzin <pierre.marzin@iot.bzh>

--- templates/machine/ebisu/50_setup.sh
+++ templates/machine/ebisu/50_setup.sh
@@ -1,10 +1,22 @@
 # setup proprietary gfx drivers and multimedia packages
 pushd $METADIR 2>/dev/null
 
-SETUP_MM_SCRIPT=$METADIR/meta-agl/meta-agl-bsp/meta-rcar-gen3/scripts/setup_mm_packages.sh
-if [ -f $SETUP_MM_SCRIPT ]; then
-	. $SETUP_MM_SCRIPT
-	copy_mm_packages
+COPY_SCRIPT="$METADIR/bsp/meta-renesas-rcar-gen3/meta-rcar-gen3/docs/sample/copyscript/copy_proprietary_softwares.sh"
+EXTRACT_DIR=$METADIR/binary-tmp
+
+if [ ! -d $EXTRACT_DIR ]; then
+	echo "ERROR: $EXTRACT_DIR does not exist." && exit 1
+else
+	[ -z "$(ls -A $EXTRACT_DIR)"  ] && echo "ERROR: $EXTRACT_DIR is empty." && exit 1
+fi
+
+if [ -f $COPY_SCRIPT ]; then
+	cd $METADIR/bsp/meta-renesas-rcar-gen3/
+	$COPY_SCRIPT $EXTRACT_DIR
+	cd ..
+else
+	echo "Script to copy Renesas proprietary drivers for $MACHINE not found."
+	exit 1
 fi
 
 popd 2>/dev/null
