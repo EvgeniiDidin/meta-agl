99e140275e93c642e167833ea4c6b60ba219a76c
Pierre Marzin <pierre.marzin@iot.bzh>
2019-06-25 09:32:27 +0200

    ebisu: Use a specified path to integrate binaries
    E3 binary should be ZIPped into one archive and the path to its location
    should be exported to the environnement before setting up the AGL build.
    
    Bug-AGL: SPEC-2564
    
    Change-Id: Ie26ba0246e7fbd0fe923fcbe0f4b4160c6d7e0f2
    Signed-off-by: Pierre Marzin <pierre.marzin@iot.bzh>

--- templates/machine/ebisu/50_setup.sh
+++ templates/machine/ebisu/50_setup.sh
@@ -3,19 +3,34 @@ pushd $METADIR 2>/dev/null
 
 COPY_SCRIPT="$METADIR/bsp/meta-renesas-rcar-gen3/meta-rcar-gen3/docs/sample/copyscript/copy_proprietary_softwares.sh"
 EXTRACT_DIR=$METADIR/binary-tmp
+#EBISU_BIN_PATH should contain the path where the .zip archive of E3 binaries is.
 
-if [ ! -d $EXTRACT_DIR ]; then
-	echo "ERROR: $EXTRACT_DIR does not exist." && exit 1
+# Check the ebisu binaries path
+if [[ ! -d $EBISU_BIN_PATH ]] || [[ $EBISU_BIN_PATH == "" ]]; then
+	echo "ERROR: E3 Binary path not valid."
+	echo "HELP: Export the path where the E3 Binaries ZIP file is into 'EBISU_BIN_PATH' then launch the setup again."
+	echo "HELP: Example: '$ export EBISU_BIN_PATH=`pwd`/ebisu_binaries'"
+	exit 1
 else
-	[ -z "$(ls -A $EXTRACT_DIR)"  ] && echo "ERROR: $EXTRACT_DIR is empty." && exit 1
+	[ -z "$(ls -A $EBISU_BIN_PATH)" ] && echo "ERROR: $EBISU_BIN_PATH is empty. Add the E3 Binaries ZIP file inside and try again." && exit 1
 fi
 
 if [ -f $COPY_SCRIPT ]; then
+	# Extract the ZIP into the tmp directory
+	mkdir -p $EXTRACT_DIR
+	unzip -q -o $EBISU_BIN_PATH/*.zip -d $EXTRACT_DIR
+
 	cd $METADIR/bsp/meta-renesas-rcar-gen3/
 	$COPY_SCRIPT $EXTRACT_DIR
 	cd ..
+
+	#Fix libpvrWAYLAND_WSEGL.so
+	#TODO
+
+	#Clean temp dir
+	rm -r $EXTRACT_DIR
 else
-	echo "Script to copy Renesas proprietary drivers for $MACHINE not found."
+	echo "ERROR: Script to copy Renesas proprietary drivers for $MACHINE not found."
 	exit 1
 fi
 
