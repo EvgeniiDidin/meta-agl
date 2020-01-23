7819fc880138c4c89a098e872cb671ae25f42a1a
Ronan Le Martret <ronan.lemartret@iot.bzh>
2020-01-23 22:14:28 +0100

    Fix SOTA build for build other than qemu
    - The meta-updater-qemux86-64 must be add only for qemu build.
    
    Change-Id: Iebdeddd869b2fca88467acd6983826b4f7e49de1
    Bug-AGL: SPEC-3097
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

---
+++ templates/feature/agl-sota/50_setup.sh
@@ -0,0 +1,24 @@
+
+cat <<EOF >> ${BUILDDIR}/conf/bblayers.conf
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# fragment {
+# ${METADIR}/meta-agl/templates/feature/agl-sota/50_setup.sh
+#
+EOF
+
+case ${MACHINE} in
+	"qemux86-64")
+	  echo "BBLAYERS =+ \"\${METADIR}/external/meta-updater-qemux86-64\"" >> ${BUILDDIR}/conf/bblayers.conf;;
+	*)
+	  echo "#No extra SOTA feature layer for MACHINE ${MACHINE}" >> ${BUILDDIR}/conf/bblayers.conf;;
+esac
+
+
+cat <<EOF >> ${BUILDDIR}/conf/bblayers.conf
+
+#
+# }
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+EOF
