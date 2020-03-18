EvgeniiDidin/meta-agl

commit b1c5541c24b1e64d84826df6c0e573ab2c83a4fe

bluez: workaround aclmtu issue on OBEX transfers
Default MTU of 310 causes PBAP/OBEX transactions to timeout before
completing the entire transfer from coming from paired iOS device.

Change this to 930 to avoid timeouts. This is a workaround till
a better solution is devised.

Bug-AGL: SPEC-3206
Change-Id: I48bdc6107ac9176397bc0137496946c42b1c95d7
Signed-off-by: Matt Ranostay <matt.ranostay@konsulko.com>

meta-agl-profile-core/recipes-connectivity/bluez5/bluez5_%.bbappend
@@ -3,6 +3,7 @@ SRC_URI_append = " \
     file://bluetooth.conf \
     file://tmpdir.conf \
     file://obex.service \
+    file://bluetooth.service \
     file://0001-obex-report-notification-status-on-incoming-message.patch \
 "
 
@@ -18,5 +19,7 @@ do_install_append() {
         install -m 0644 ${WORKDIR}/tmpdir.conf ${D}${systemd_user_unitdir}/obex.service.d/tmpdir.conf
         mkdir -p ${D}/etc/systemd/user
         ln -sf ${systemd_user_unitdir}/obex.service ${D}/etc/systemd/user/dbus-org.bluez.obex.service
+        mkdir -p ${systemd_system_unitdir}
+        install -m 0644 ${WORKDIR}/bluetooth.service ${D}${systemd_system_unitdir}
     fi
 }

meta-agl-profile-core/recipes-connectivity/bluez5/bluez5/bluetooth.service
@@ -1,0 +1,21 @@
+[Unit]
+Description=Bluetooth service
+Documentation=man:bluetoothd(8)
+ConditionPathIsDirectory=/sys/class/bluetooth
+
+[Service]
+Type=dbus
+BusName=org.bluez
+ExecStartPre=/usr/bin/hciconfig hci0 aclmtu 930:10
+ExecStart=/usr/libexec/bluetooth/bluetoothd
+NotifyAccess=main
+#WatchdogSec=10
+#Restart=on-failure
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+LimitNPROC=1
+ProtectHome=true
+ProtectSystem=full
+
+[Install]
+WantedBy=bluetooth.target
+Alias=dbus-org.bluez.service

commit 2183bcb597c78ed0ed7b21aef0c5d19e858535ab

Fix build issue in bluez5 bbappend file
The commit b1c5541c2 introduced a 'mkdir -p ${systemd_system_unitdir}'
which should be prefixed with ${D}.

It produces a build error (permission denied on do_install).

Fix it.

Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>
Change-Id: Ife8b374379937e87cf3c045f105c3eb121f0941d

meta-agl-profile-core/recipes-connectivity/bluez5/bluez5_%.bbappend
@@ -19,7 +19,7 @@ do_install_append() {
         install -m 0644 ${WORKDIR}/tmpdir.conf ${D}${systemd_user_unitdir}/obex.service.d/tmpdir.conf
         mkdir -p ${D}/etc/systemd/user
         ln -sf ${systemd_user_unitdir}/obex.service ${D}/etc/systemd/user/dbus-org.bluez.obex.service
-        mkdir -p ${systemd_system_unitdir}
+        mkdir -p ${D}${systemd_system_unitdir}
         install -m 0644 ${WORKDIR}/bluetooth.service ${D}${systemd_system_unitdir}
     fi
 }